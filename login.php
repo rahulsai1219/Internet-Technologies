8e02a6e6a9c6c048d3065a81c92f8547a58d4347
rahulsai1219 <38484825+rahulsai1219@users.noreply.github.com>
2018-04-18 09:45:08 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+$con = mysqli_connect("localhost", "root", "tiger","lab") or die ("could not connect to database");
+$name=$_POST["name"];
+$password=$_POST["password"];
+$qry="SELECT * from login where name='$name' AND password='$password' ";
+$result=mysqli_query($con,$qry);
+if($result) 
+{
+	if(mysqli_num_rows($result) > 0)
+	{
+		echo"Login Successfull !!!";
+	}
+	else
+	{
+		echo"invalid username or password";
+	}
+}
+
+?>
